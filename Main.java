66ff39a96c0312ff10e9ee53cbfd002e4684b175
pointbazaar <alex23667@gmail.com>
2019-10-09 00:26:58 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,4 +10,13 @@ class Pos{
   public String toString(){    
       return "["+this.row+","+this.col+"]";
   }
+  @Override
+  public boolean equals(Object other){
+      if(other instanceof Pos){
+          if(other.row==this.row && other.col == this.col){
+              return true;
+          }
+      }
+      return false;
+  }
 }
